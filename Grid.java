511aea324df81d2496e31bbffc8fe9b8e0a6faf3
Bilun <epilcher126@gmail.com>
2016-03-28 16:11:42 -0700

    a commit!

--- Grid.java
+++ Grid.java
@@ -22,4 +22,22 @@ public class Grid {
     public int getHeight() {
         return height;
     }
+    
+    
+    public boolean loadGame(String saveName){
+    	saveDir="TBD";
+    	FileWriter output = new FileWriter(saveDir);
+    	BufferedWriter outputWrapper = new BufferedWriter(output);
+    	gameWidth=this.getWidth();
+    	gameHeight=this.getHeight();
+    	for(int squareX=0;squareX<gameWidth;squareX++){
+    		for(nt squareY=0;squareY<gameHeight;squareY++){
+    			
+    		}
+    	}
+    }
+    public boolean saveGame(String saveName){
+    	
+    }
+    
 }
