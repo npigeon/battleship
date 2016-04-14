b7b2f7e8aa07fe1b5efd78ae5f143c139a32faff
Bilun <epilcher126@gmail.com>
2016-04-13 17:16:01 -0700

    this may work
    clean wipe in a new branch

--- BattleShip.java
+++
@@ -1,9 +0,0 @@
-public class BattleShip {
-    public static Grid gameBoard = new Grid(12);
-    public static GridViewer viewManager = new GridViewer();
-
-    public static void main(String args[] ) {
-        viewManager.displayGrid(gameBoard);
-        
-    }
-}
--- Square.java
+++
@@ -1,9 +0,0 @@
-public class Square {
-    private boolean occupiedByOpponent;
-    private boolean occupiedBySelf;
-    private boolean hasBeenFiredAt;
-    public char getDisplayCharacter () {
-    	
-        return 'o';
-    }
-}
