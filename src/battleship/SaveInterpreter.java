9073946b51596ae74834704ed0dae7e7f2937b30
Bilun <epilcher126@gmail.com>
2016-04-13 19:15:50 -0700

    bugfixes
    bugfixes to Ship Class

--- src/battleship/SaveInterpreter.java
+++ src/battleship/SaveInterpreter.java
@@ -1,5 +1,90 @@
-package battleship;
+public class SaveInterpreter(){
+	public int gridWidth;
+	public int gridHeight;
+	
+	public SaveInterpreter(Grid gameGrid){
+		gridWidth=gameGrid.getWidth();
+		gridHeight=gameGrid.getHeight();
+		
+	}
+	
+	public boolean enumerateSquare(Integer xPos, Integer yPos){
+    	//converts x & y coodinates to unique enumeration and returns that integer
+		//enumerates starting from  the top left, left to right, down to the next row after finishing the previous row
+		//NOTE: top left square is enumerated 1, not 0
+				
+		
+    	return gameWidth*yPos+xPos+1;  //add 1 because the indexing needs to start from 1 rather then 0 
+    	                               //since formatSave will denote a miss/hit/unguessed square by
+    	                               //multiplying the enumeration of the square by -1/1/0 ;so a 0 result
+    	                               //needs to be necessarily an unguessed square.  Better to start indexing 
+    	                               //from 1 then have cumbersome conversions
+    	                               
+    }
+    public boolean enumerateSquare(int enumeration){
+    	//converts enumeration back to x & y coorrdinates and returns an arraylist of those coodinates
+    	//reverse operation of the first definition of this method
+    	ArrayList<integer> coordinates= new ArrayList<integer>();
+    	
+    	
+    	xPos=(enumeration-1)%gameWidth;
+    	yPos=(enumeration-1)/gameWidth;  //integer division rounds down, so dividing by width will give correct row
+    	//node the -1 is to compensate for the indexing is starting from 1 rather then 0
+    	
+    	coordinates.add(xPos);
+    	coordinates.add(yPos);
+    	return coordinates;
+    }
+    
+	public formatSave(Grid gameGrid){
+		//formats gamestate into a list of 4 strings
+		ArrayList<String> outputRows = new ArrayList<String>();
+		
+		//*******
+		//first string: board size, formated as width dimenion, followed by a comma, followed by height dimension
+		outputRows.add(Integer.toString(gridWidth)+","+Integer.toString(gridHeight));
+		
+		//*******
+    	//second string: lists all previous guesses by player, each guess as the enumeration of the square,
+    	//positive if a hit, negative if a miss, each guess seperated from the next by a comma
+		
+		String guesses = "";
+		
+		Integer currSquareEnum = this.enumerateSquare(0,0); //top left square handled outside loop so no extra  leading comma
+		Integer currSquareStatus=gameGrid.getSquare(0,0).getDisplayCharacter();
+		// <<code for connverting result of getDisplay character to -1 for miss, 0 for unguessed, 1 for hit goes here>>
+		
+		Integer codedEnum=currSquareStatus*currSquareEnum;
+		if  (codedEnum!=0){  
+			guesses=(Integer.toString(codedEnum))
+		}
+			
+		
+		for(Integer squareY=0;squareY<gridHeight;squareY++){
+    		for(Integer squareX=1;squareX<gridWidth;squareX++){ 
+    			// iterates over all remaining squares, performing same operation as was performed on top left square
+    			
+    			Integer currSquareEnum = this.enumerateSquare(squareX,squareY);
+    			Integer currSquareStatus=gameGrid.getSquare(squareX,squareY).getDisplayCharacter();
+    			// <<code for connverting result of getDisplay character to -1 for miss, 0 for unguessed, 1 for hit goes here>>
+    			
+    			Integer codedEnum=currSquareStatus*currSquareEnum;
+    			if  (codedEnum!=0){
+    				guesses+=(","+Integer.toString(codedEnum))
+    			}
+    	
+    	
+    	
+    	
+    		}
+    	}
+		outputRows.add(guesses);
+    	
 
-public class SaveInterpreter {
-
+    	//*******
+    	//third string: lists the enumeration of each square occupied by the player's ships
+		
+	}
+	
+	
 }
--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -1,5 +1,182 @@
 package battleship;
 
-public class Ship {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Ship{
+	List<Coordinates> shipLocation = new ArrayList<Coordinates>();
+	
+	public Ship(List<Coordinates> shipLoc){
+		for(Coordinates shipSegmentLoc:shipLoc){
+			shipLocation.add(shipSegmentLoc);
+		}
+	}
+
+	public List<Coordinates> sortCoordsByX(List<Coordinates> coordList){
+		// method for ordering the coordinates in shipLocation in ascending order by horizantal/x coordinate
+		// based on merge sort recursive algorithm
+
+		int length=coordList.size(); 
+		List<Coordinates> combinedList=new ArrayList<Coordinates>(); //our output variable
+		
+		if (length==1){  //method just returns input if of length 1
+			             //important because a list of size 1 is always sorted
+			combinedList=coordList;
+		}
+		else if(length>=2){
+		//otherwise divides list into two roughly equal sublists and applies this method to each of those
+		//sublists.
+			
+			int midPoint=length/2;
+			
+			
+			//recursion used to evaluating 
+			//start from single element lists, then solve 2 element listed, then 4 and so on
+			
+			List<Coordinates> subList1=new ArrayList<Coordinates>(coordList.subList(0, midPoint));
+			List<Coordinates> sortedSub1=this.sortCoordsByX(subList1);//evulates first half sublist first
+			
+			
+			List<Coordinates> subList2=new ArrayList<Coordinates>(coordList.subList(midPoint, length));
+			List<Coordinates> sortedSub2=this.sortCoordsByX(subList2);//resursion for second half sublist
+			
+			
+			
+			while((sortedSub1.size()!=0)||(sortedSub2.size()!=0)){
+				//assuming both sortedSub1 & sortedSub2 are already sorted this will loop will merge
+				//the two into a single sorted list by repeatedly adding the lesser of the two list's
+				//mimimal value to combined list and removing them from their origonal list.  
+				// Loop ends when sorted lists are empty
+				if(sortedSub1.size()==0){
+					
+					combinedList.add(sortedSub2.get(0));
+					sortedSub2.remove(0);
+				}
+				else if(sortedSub2.size()==0){
+					combinedList.add(sortedSub1.get(0));
+					sortedSub1.remove(0);
+					
+				}
+				else if(sortedSub1.get(0).getX()<=sortedSub2.get(0).getX()){
+					combinedList.add(sortedSub1.get(0));
+					
+
+					sortedSub1.remove(0);
+				}
+				else{
+					combinedList.add(sortedSub2.get(0));
+					
+					sortedSub2.remove(0);
+				}
+				
+					
+			}
+			
+		}
+
+	return combinedList;  //returns sorted version of currently evaluated sublist
+}
+	public List<Coordinates> sortCoordsByY(List<Coordinates> coordList){
+		// method for ordering the coordinates in shipLocation in ascending order by vertical/y coordinate
+		// based on merge sort recursive algorithm
+		// sortCoordinatesByX has more detailed comments on the method
+
+		int length=coordList.size();
+		List<Coordinates> combinedList=new ArrayList<Coordinates>();
+		
+		if (length==1){
+			combinedList=coordList;
+		}
+		else if(length>=2){
+		
+			
+			int midPoint=length/2;
+			
+			
+			List<Coordinates> subList1=new ArrayList<Coordinates>(coordList.subList(0, midPoint));
+			List<Coordinates> sortedSub1=this.sortCoordsByY(subList1);
+			
+			List<Coordinates> subList2=new ArrayList<Coordinates>(coordList.subList(midPoint, length));
+			List<Coordinates> sortedSub2=this.sortCoordsByY(subList2);
+
+			
+			
+			while((sortedSub1.size()!=0)||(sortedSub2.size()!=0)){
+				if(sortedSub1.size()==0){
+					
+					combinedList.add(sortedSub2.get(0));
+					sortedSub2.remove(0);
+				}
+				else if(sortedSub2.size()==0){
+					combinedList.add(sortedSub1.get(0));
+					sortedSub1.remove(0);
+					
+				}
+				else if(sortedSub1.get(0).getY()<=sortedSub2.get(0).getY()){
+					combinedList.add(sortedSub1.get(0));
+					
+
+					sortedSub1.remove(0);
+				}
+				else{
+					combinedList.add(sortedSub2.get(0));
+					
+					sortedSub2.remove(0);
+				}
+				
+					
+			}
+			
+		}
+
+	return combinedList;
+}
+	
+	
+	
+	public boolean checkValidity(){
+		//Checks that ship is actually in a contiguous straight line
+		Integer possibleCol=shipLocation.get(0).getX();
+		Integer possibleRow=shipLocation.get(0).getY();
+		
+		
+		
+		boolean valid=true;
+		
+		this.sortCoordsByX(shipLocation);
+		
+		for(Coordinates shipSegLoc:shipLocation){  //tests if all entries have the same solumn entry
+			if(shipSegLoc.getX()!=possibleCol){
+				valid=false
+				break;
+			}
+		}
+		if (valid==true){
+			return true;
+		}
+		else{
+			valid=true;
+			self.sortByY();
+		}
+		
+		
+		
+		for(Coordinates shipSegLoc:shipLocation){
+			if((shipSegLoc.getY()!=possibleRow) ||){
+				valid=false
+				break;
+			}
+		}
+		if (valid==true){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	private void sortCoordsByX() {
+		// TODO Auto-generated method stub
+		
+	}
 }
